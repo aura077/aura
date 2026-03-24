8cfb53ff0edf107b2342b43d2fd8d5534c808c75
hyparkio <hypark.github.io@gmail.com>
2026-03-24 14:48:42 +0500

    Fresh start with new API

--- firebase-config.js
+++ firebase-config.js
@@ -3,16 +3,15 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBagrHgtTIObFHCK-XQ-Fs_T-fbwN6Xed8",
-  authDomain: "aura-62244.firebaseapp.com",
-  projectId: "aura-62244",
-  storageBucket: "aura-62244.firebasestorage.app",
-  messagingSenderId: "617932120986",
-  appId: "1:617932120986:web:f26dc847282eebdbd1d023",
-  measurementId: "G-3Z7DFPZ08G"
+  apiKey: "AIzaSyCFikZSzeiVbulbnjRm5um4rLkJ03wmCQw",
+  authDomain: "aura-077.firebaseapp.com",
+  projectId: "aura-077",
+  storageBucket: "aura-077.firebasestorage.app",
+  messagingSenderId: "465157338528",
+  appId: "1:465157338528:web:1db123ac3777237ba4e0c5",
+  measurementId: "G-DXZKH4HDKP"
 };
 
-// Инициализация
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
